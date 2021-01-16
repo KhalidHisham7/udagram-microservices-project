c848271d09a8127233d6a616a2aecd6a1e8d2351
unknown <timoncr9@gmail.com>
2021-01-16 18:36:37 +0200

    final edits for submission

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9cf386eeaf9343aca9b05d6a5e561f4-1956260085.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
